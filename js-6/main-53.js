2502c63a627512a5f578925579586c67ac15415e
Alik186 <sania.9889@mail.ru>
2026-09-20 14:47:36 +0500

    Add files via upload

---
+++ js-6/main-53.js
@@ -0,0 +1,49 @@
+// Задание 1.
+// Дан массив пользователей:
+// const users = [
+//   { name: 'Alex', age: 24, isAdmin: false },
+//   { name: 'Bob', age: 13, isAdmin: false },
+//   { name: 'John', age: 31, isAdmin: true },
+//   { name: 'Jane', age: 20, isAdmin: false },
+//]
+// Добавьте в конец массива двух пользователей:
+// { name: 'Ann', age: 19, isAdmin: false },
+// { name: 'Jack', age: 43, isAdmin: true }
+
+// Задание 2.
+// Используя массив пользователей users из предыдущего задания, напишите функцию getUserAverageAge(users), которая возвращает средний возраст пользователей.
+
+// Задание 3.
+// Используя массив пользователей users из предыдущего задания, напишите функцию getAllAdmins(users), которая возвращает массив всех администраторов.
+
+// Задание 4.
+// Напишите функцию first(arr, n), которая возвращает первые n элементов массива. Если n == 0, возвращается пустой массив [], если n == undefined, то возвращается массив с первым элементом.
+
+//Задание 1
+
+const users = [
+	{ name: "Alex", age: 24, isAdmin: false },
+	{ name: "Bob", age: 13, isAdmin: false },
+	{ name: "John", age: 31, isAdmin: true },
+	{ name: "Jane", age: 20, isAdmin: false },
+];
+users.push({ name: "Ann", age: 19, isAdmin: false });
+users.push({ name: "Jack", age: 43, isAdmin: true });
+
+//Задание 2
+
+function getUserAverageAge(users) {
+	const totalAge = users.reduce((acc, user) => acc + user.age, 0);
+	return totalAge / users.length;
+}
+
+//Задание 3
+function getAllAdmins(users) {
+	return users.filter((user) => user.isAdmin);
+}
+
+//Задание 4
+function first(arr, n) {
+	if (n === undefined) return [arr[0]];
+	return arr.slice(0, n);
+}
